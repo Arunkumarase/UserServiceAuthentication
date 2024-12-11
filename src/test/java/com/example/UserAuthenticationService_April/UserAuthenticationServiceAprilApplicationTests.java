d8e1b9352c01f34e82fe947710a1f15ec0e2008f
Arunkumar <arunthangaraj11@gmail.com>
2024-12-10 22:41:37 -0500

    features updated

--- src/test/java/com/example/UserAuthenticationService_April/UserAuthenticationServiceAprilApplicationTests.java
+++ src/test/java/com/example/UserAuthenticationService_April/UserAuthenticationServiceAprilApplicationTests.java
@@ -1,10 +1,10 @@
-package userauthenticationservice;
+package com.example.UserAuthenticationService_April;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserauthenticationserviceApplicationTests {
+class UserAuthenticationServiceAprilApplicationTests {
 
 	@Test
 	void contextLoads() {
